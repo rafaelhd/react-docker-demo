c282543d682e7b14e48eaff267e44eaabdbd57bc
Rafael <rafaelhd89@hotmail.com>
2020-07-16 11:34:12 -0500

    changed main text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            vas chingon we no te rindas!
+            PRUEBA!!!
         </p>
         <a
           className="App-link"
